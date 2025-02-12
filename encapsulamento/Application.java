382abf34da448cced9fafdd1b8f5c6922dea4d84
Guylherme Emmanuel <getqueiroz@usp.br>
2025-02-11 21:51:41 -0300

    fim aula encapsulamento

--- encapsulamento/Application.java
+++ encapsulamento/Application.java
@@ -3,7 +3,12 @@ import src.main.Contato;
 public class Application {
     public static void main(String[] args) {
         Contato contato = new Contato("Guylherme", "2345678");
+        System.out.println(contato);
+        
+        Contato contato2 = new Contato("José", "999999999", "Rua Imperial");
+        System.out.println(contato2);
 
-        contato.setNome("José");
+        contato.setEndereco("Av. Bady Bassit");
+        System.out.println(contato);
     }
 }
--- encapsulamento/src/main/Contato.java
+++ encapsulamento/src/main/Contato.java
@@ -3,12 +3,20 @@ package src.main;
 public class Contato {
     private String nome;
     private String telefone;
+    private String endereco;
+
+    public Contato(String nome, String telefone, String endereco) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
 
     public Contato(String nome, String telefone) {
         this.nome = nome;
         this.telefone = telefone;
     }
 
+    // getters and setters
     public String getNome() {
         return this.nome;
     }
@@ -17,7 +25,28 @@ public class Contato {
         return this.telefone;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setNome(String newNome) {
+        this.nome = newNome;
+    }
+
+    public void setTelefone(String newTelefone) {
+        this.telefone = newTelefone;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String toString() {
+        String enderecoStr = "Nome: " + this.nome + ", Telefone: " + this.telefone;
+        if(this.endereco != null) {
+            enderecoStr += ", Endereço: " + this.endereco;
+        }
+
+        return enderecoStr;
     }
 }
